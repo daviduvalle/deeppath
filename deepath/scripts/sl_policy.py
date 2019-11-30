1e05bc8d8705f82c25dc38538c73b74d726fa122
daviduvalle <david.uvalle@gmail.com>
2019-11-29 19:56:33 -0800

    Load data only once instead of each episode
    This change also swaps tabs in favor of 4
    spaces on the files I touched

--- deepath/scripts/sl_policy.py
+++ deepath/scripts/sl_policy.py
@@ -1,5 +1,5 @@
 from __future__ import division
-import tensorflow as tf 
+import tensorflow as tf
 import numpy as np
 from itertools import count
 import sys
@@ -16,120 +16,125 @@ relation = sys.argv[1]
 graphpath = dataPath + 'tasks/' + relation + '/' + 'graph.txt'
 relationPath = dataPath + 'tasks/' + relation + '/' + 'train_pos'
 
+
 class SupervisedPolicy(object):
-	"""docstring for SupervisedPolicy"""
-	def __init__(self, learning_rate = 0.001):
-		self.initializer = tf.contrib.layers.xavier_initializer()
-		with tf.variable_scope('supervised_policy'):
-			self.state = tf.placeholder(tf.float32, [None, state_dim], name = 'state')
-			self.action = tf.placeholder(tf.int32, [None], name = 'action')
-			self.action_prob = policy_nn(self.state, state_dim, action_space, self.initializer)
+    """docstring for SupervisedPolicy"""
 
-			action_mask = tf.cast(tf.one_hot(self.action, depth = action_space), tf.bool)
-			self.picked_action_prob = tf.boolean_mask(self.action_prob, action_mask)
+    def __init__(self, learning_rate=0.001):
+        self.initializer = tf.contrib.layers.xavier_initializer()
+        with tf.variable_scope('supervised_policy'):
+            self.state = tf.placeholder(tf.float32, [None, state_dim], name='state')
+            self.action = tf.placeholder(tf.int32, [None], name='action')
+            self.action_prob = policy_nn(self.state, state_dim, action_space, self.initializer)
 
-			self.loss = tf.reduce_sum(-tf.log(self.picked_action_prob)) + sum(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES, scope = 'supervised_policy'))
-			self.optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate)
-			self.train_op = self.optimizer.minimize(self.loss)
+            action_mask = tf.cast(tf.one_hot(self.action, depth=action_space), tf.bool)
+            self.picked_action_prob = tf.boolean_mask(self.action_prob, action_mask)
 
-	def predict(self, state, sess = None):
-		sess = sess or tf.get_default_session()
-		return sess.run(self.action_prob, {self.state: state})
+            self.loss = tf.reduce_sum(-tf.log(self.picked_action_prob)) + sum(
+                tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES, scope='supervised_policy'))
+            self.optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+            self.train_op = self.optimizer.minimize(self.loss)
 
-	def update(self, state, action, sess = None):
-		sess = sess or tf.get_default_session()
-		_, loss = sess.run([self.train_op, self.loss], {self.state: state, self.action: action})
-		return loss
+    def predict(self, state, sess=None):
+        sess = sess or tf.get_default_session()
+        return sess.run(self.action_prob, {self.state: state})
+
+    def update(self, state, action, sess=None):
+        sess = sess or tf.get_default_session()
+        _, loss = sess.run([self.train_op, self.loss], {self.state: state, self.action: action})
+        return loss
+
 
 def train():
-	tf.reset_default_graph()
-	policy_nn = SupervisedPolicy()
-	import os
-	print('The python is path is', os.environ.get("PYTHONPATH"))
-	print('The current working directory is', os.getcwd())
+    tf.reset_default_graph()
+    policy_nn = SupervisedPolicy()
+    import os
+    print('The python is path is', os.environ.get("PYTHONPATH"))
+    print('The current working directory is', os.getcwd())
 
+    f = open(relationPath)
+    train_data = f.readlines()
+    f.close()
 
-	f = open(relationPath)
-	train_data = f.readlines()
-	f.close()
+    num_samples = len(train_data)
 
-	num_samples = len(train_data)
+    saver = tf.train.Saver()
+    env = Env(dataPath)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        if num_samples > 500:
+            num_samples = 500
+        else:
+            num_episodes = num_samples
 
-	saver = tf.train.Saver()
-	with tf.Session() as sess:
-		sess.run(tf.global_variables_initializer())
-		if num_samples > 500:
-			num_samples = 500
-		else:
-			num_episodes = num_samples
+        for episode in range(num_samples):
+            print("Episode %d" % episode)
+            print('Training Sample:', train_data[episode % num_samples][:-1])
 
-		for episode in range(num_samples):
-			print("Episode %d" % episode)
-			print('Training Sample:', train_data[episode%num_samples][:-1])
+            #env = Env(dataPath, train_data[episode % num_samples])
+            env.append_task_relations(train_data[episode % num_samples])
+            sample = train_data[episode % num_samples].split()
 
-			env = Env(dataPath, train_data[episode%num_samples])
-			sample = train_data[episode%num_samples].split()
+            try:
+                good_episodes = teacher(sample[0], sample[1], 5, env, graphpath)
+            except Exception as e:
+                print('Cannot find a path')
+                continue
 
-			try:
-				good_episodes = teacher(sample[0], sample[1], 5, env, graphpath)
-			except Exception as e:
-				print( 'Cannot find a path')
-				continue
+            for item in good_episodes:
+                state_batch = []
+                action_batch = []
+                for t, transition in enumerate(item):
+                    state_batch.append(transition.state)
+                    action_batch.append(transition.action)
+                state_batch = np.squeeze(state_batch)
+                state_batch = np.reshape(state_batch, [-1, state_dim])
+                policy_nn.update(state_batch, action_batch)
 
-			for item in good_episodes:
-				state_batch = []
-				action_batch = []
-				for t, transition in enumerate(item):
-					state_batch.append(transition.state)
-					action_batch.append(transition.action)
-				state_batch = np.squeeze(state_batch)
-				state_batch = np.reshape(state_batch, [-1, state_dim])
-				policy_nn.update(state_batch, action_batch)
-
-		saver.save(sess, 'models/policy_supervised_' + relation)
-		print( 'Model saved')
+        saver.save(sess, 'models/policy_supervised_' + relation)
+        print('Model saved')
 
 
 def test(test_episodes):
-	tf.reset_default_graph()
-	policy_nn = SupervisedPolicy()
+    tf.reset_default_graph()
+    policy_nn = SupervisedPolicy()
 
-	f = open(relationPath)
-	test_data = f.readlines()
-	f.close()
+    f = open(relationPath)
+    test_data = f.readlines()
+    f.close()
 
-	test_num = len(test_data)
+    test_num = len(test_data)
 
-	test_data = test_data[-test_episodes:]
-	print (len(test_data))
-	
-	success = 0
+    test_data = test_data[-test_episodes:]
+    print(len(test_data))
 
-	saver = tf.train.Saver()
-	with tf.Session() as sess:
-		saver.restore(sess, 'models/policy_supervised_'+ relation)
-		print ('Model reloaded')
-		for episode in xrange(len(test_data)):
-			print( 'Test sample %d: %s' % (episode,test_data[episode][:-1]))
-			env = Env(dataPath, test_data[episode])
-			sample = test_data[episode].split()
-			state_idx = [env.entity2id_[sample[0]], env.entity2id_[sample[1]], 0]
-			for t in count():
-				state_vec = env.idx_state(state_idx)
-				action_probs = policy_nn.predict(state_vec)
-				action_chosen = np.random.choice(np.arange(action_space), p = np.squeeze(action_probs))
-				reward, new_state, done = env.interact(state_idx, action_chosen)
-				if done or t == max_steps_test:
-					if done:
-						print ('Success')
-						success += 1
-					print ('Episode ends\n')
-					break
-				state_idx = new_state
+    success = 0
 
-	print ('Success persentage:', success/test_episodes)
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        saver.restore(sess, 'models/policy_supervised_' + relation)
+        print('Model reloaded')
+        for episode in xrange(len(test_data)):
+            print('Test sample %d: %s' % (episode, test_data[episode][:-1]))
+            env = Env(dataPath, test_data[episode])
+            sample = test_data[episode].split()
+            state_idx = [env.entity2id_[sample[0]], env.entity2id_[sample[1]], 0]
+            for t in count():
+                state_vec = env.idx_state(state_idx)
+                action_probs = policy_nn.predict(state_vec)
+                action_chosen = np.random.choice(np.arange(action_space), p=np.squeeze(action_probs))
+                reward, new_state, done = env.interact(state_idx, action_chosen)
+                if done or t == max_steps_test:
+                    if done:
+                        print('Success')
+                        success += 1
+                    print('Episode ends\n')
+                    break
+                state_idx = new_state
+
+    print('Success persentage:', success / test_episodes)
+
 
 if __name__ == "__main__":
-	train()
-	# test(50)
-
+    train()
+# test(50)
